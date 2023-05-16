d97fbc730248646dcef3ba651f67cc40cb5bcea8
Mario Puebla Junior <mariopuebla17@gmail.com>
2023-05-16 08:16:51 -0300

    initial commit
    Mudando pastas de locais

---
+++ Migrations/20230420105531_PopulaCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, ImageUrl) values('Bebidas', 'bebidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImageUrl) values('Lanches', 'lanches.jpg')");
+            mb.Sql("Insert into Categorias(Nome, ImageUrl) values('Sobremesas', 'sobremesa.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
---
+++ Migrations/20230420105954_PopulaProdutos.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImageUrl, Estoque, DataCadastro, CategoriaId) VALUES('Coca-Cola Zero', 'Refrigerante 350ml', 5.45, 'cocacola.jpg', 50, now(), 1)");
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImageUrl, Estoque, DataCadastro, CategoriaId) VALUES('Lanche de Atum', 'Lanche de Atum com Maionese', 8.50, 'atum.jpg', 10, now(), 2)");
+            mb.Sql("INSERT INTO Produtos(Nome, Descricao, Preco, ImageUrl, Estoque, DataCadastro, CategoriaId) VALUES('Pudim 100g', 'Pudim de leite condensado 100g', 6.75, 'pudim.jpg', 20, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
